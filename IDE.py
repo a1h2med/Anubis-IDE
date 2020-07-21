55e93b6125112106e346a80e87b93b5f6eea0c71
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2020-07-22 00:27:49 +0200

    Update IDE.py

--- IDE.py
+++ IDE.py
@@ -5,7 +5,7 @@
 
 import sys
 import glob
-#import serial
+import serial
 
 import Python_Coloring
 from PyQt5 import QtCore
@@ -14,7 +14,32 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from pathlib import Path
 
+def serial_ports():
+    """ Lists serial port names
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 
 #
@@ -241,11 +266,11 @@ class UI(QMainWindow):
         # copyrights of serial_ports() function goes back to a guy from stackoverflow(whome I can't remember his name), so thank you (unknown)
         Port_Action = QMenu('port', self)
 
-#        res = serial_ports()
+        res = serial_ports()
 
-#        for i in range(len(res)):
-#            s = res[i]
-#            Port_Action.addAction(s, self.PortClicked)
+        for i in range(len(res)):
+            s = res[i]
+            Port_Action.addAction(s, self.PortClicked)
 
         # adding the menu which I made to the original (Port menu)
         Port.addMenu(Port_Action)
@@ -348,34 +373,3 @@ if __name__ == '__main__':
     ex = UI()
     # ex = Widget()
     sys.exit(app.exec_())
-    
-    
-    
-'''    
-def serial_ports():
-    """ Lists serial port names
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-'''
