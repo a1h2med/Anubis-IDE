819d03d48f3d0d67aec88e0d9cb1598702ecd6a5
Strykar1 <ahmedms.ftw@gmail.com>
2022-06-15 17:14:54 +0200

    added test files for C# and Python codes to test highlighting

---
+++ examples/main.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    // A class to hold the main function
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String to be printed
+            string message = "Hello World!!";
+
+            // actual print function
+            Console.WriteLine(message);
+        }
+    }
+}
